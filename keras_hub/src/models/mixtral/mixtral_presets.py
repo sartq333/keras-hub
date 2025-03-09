40688c48c094110cc0e68c6fdb6551e185eaa464
sartq333 <sarthak.jain.min21@itbhu.ac.in>
2025-03-09 12:11:53 +0530

    setting up preset

--- keras_hub/src/models/mixtral/mixtral_presets.py
+++ keras_hub/src/models/mixtral/mixtral_presets.py
@@ -1,1 +1,15 @@
-backbone_presets = {}
+"""Mixtral model preset configurations."""
+
+# Metadata for loading pretrained model weights.
+backbone_presets = {
+    "gpt2_base_en": {
+        "metadata": {
+            "description": (
+                "mixtral model"
+            ),
+            "params": 56000000000 ,
+            "path": "mixtral",
+        },
+        "kaggle_handle": "kaggle://keras/mistral/keras/mixtral_8x7b-instruct-v0.1-hf/1",
+    }
+}
